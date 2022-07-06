b3c7df9b2b2443c2a04a5c661b3eff178fb542e7
zytsev <zaes2000@mail.ru>
2022-07-06 17:21:55 +0300

    feat: add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  const numModule = Math.abs(n);
+  const numStr = String(numModule);
+  let i = 0;
+  let result = '';
+  while (i < numStr.length) {
+    result = `${numStr[i]}${result}`;
+    i += 1;
+  }
+  return Number(result);
+
 }
